cbe02a5aa6d414af2e70821d99dcece8b34b99e7
arac <rachmananita12@gmail.com>
2018-09-13 20:30:31 +0700

    Login

--- Judul.js
+++ Judul.js
@@ -4,7 +4,16 @@ import {Text} from 'react-native'
 class Judul extends Component{
 	render(){
 		return (
+		
+			<View>
+			<Image source={require('./anita.jpg')} style={{width:140, height:200}} />
+			 <Text>Nama : Anita Rachman</Text>
+       		 <Text>Kelas : XI RPL 3</Text>
+      		 <Text>No Absen : 06</Text>
+     	  	 <Text>TTL : Jakarta, 23 Januari 2002</Text>
+      		 <Text>Hobby : Nonton </Text>
 			<Text style={tulisan.judul}>{this.props.title}</Text>
+			</View>
 		)
 	}
 }
